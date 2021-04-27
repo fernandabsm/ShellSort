80155a14fb232ce545ddbb93816694402cb2ae49
Fernanda Bernardes da Silva Melo <74023503+fernandabsm@users.noreply.github.com>
2021-04-27 18:01:28 -0300

    Add files via upload

---
+++ ShellSort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void Shell_Sort(char* A, int n)
+{
+	int i, j, h = 1, x;
+	do
+	{
+		h = h * 3 + 1;
+	} while (h < n);
+	do
+	{
+		h /= 3;
+		for (i = h + 1; i <= n; i++)
+		{
+			x = A[i];
+			j = i;
+			while (A[j - h] > x)
+			{
+				A[j] = A[j - h];
+				j -= h;
+				if (j <= h)
+					break;
+			}
+			A[j] = x;
+			printf("H = %d \t", h);
+			for (int i = 1; i <= 12; i++)
+			{
+				printf("%c ", A[i]);
+			}
+			printf("\n");
+		}
+	} while (h != 1);
+}
+
+int main()
+{
+	char A[13];
+	A[1] = 'F';
+	A[2] = 'E';
+	A[3] = 'R';
+	A[4] = 'N';
+	A[5] = 'A';
+	A[6] = 'N';
+	A[7] = 'D';
+	A[8] = 'A';
+	A[9] = 'M';
+	A[10] = 'E';
+	A[11] = 'L';
+	A[12] = 'O';
+
+	Shell_Sort(A, 12);
+	for (int i = 1; i <= 12; i++)
+	{
+		printf("%c ", A[i]);
+	}
+}
